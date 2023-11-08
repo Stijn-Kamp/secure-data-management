207ddff7e11fdd69eb60156e0bf41767b4969366
Thomas van den Berg <thomasvdb2002@protonmail.com>
2023-11-08 21:43:50 +0100

    debug

--- test.py
+++ test.py
@@ -9,37 +9,31 @@ class Person:
         (self.pk, self.sk, self.keyindex) = mpeck.generate_key()
 
     def upload(self, other_person, document: str, keywords: [str]):
-        return self.mpeck.add_doc(other_person, keywords, document)
+        self.server.upload(self.mpeck.add_doc(other_person, keywords, document))
 
-    def trapdoor(self, keywords):
-        return self.mpeck.trapdoor(self.sk, keywords)
+    def search(self, keywords):
+        texts = self.server.search(self.pk, self.mpeck.trapdoor(self.sk, keywords))
+        for text in texts:
+            print(self.mpeck.decrypt(self.sk, *text))
 
 
-def Consultant(Person):
+class Consultant(Person):
     def __init__(self, mpeck: MPECK, server: Server):
-        super.__init__(mpeck, server)
+        super().__init__(mpeck, server)
         print(self.pk, self.sk, self.keyindex)
         self.clients = []
 
     def upload(self, clientindex: int, document: str, keywords: [str]):
-        return super().upload([self.clients[clientindex], (super().pk, super().keyindex)], document, keywords)
-
-    def search(self, keywords):
-        trapdoor = super().trapdoor(keywords)
-        super().server.search(super().pk, trapdoor)
+        self.upload([self.clients[clientindex], (super().pk, super().keyindex)], document, keywords)
 
 
-def Client(Person):
+class Client(Person):
     def __init__(self, mpeck: MPECK, server: Server, consultant_key):
-        super.__init__(mpeck, server)
+        super().__init__(mpeck, server)
         self.consultant_pk, self.consultant_keyindex = consultant_key
 
     def upload(self, document: str, keywords: [str]):
-        return super().upload([(self.consultant_pk, self.consultant_keyindex), (super().pk, super().keyindex)], document, keywords)
-
-    def search(self, keywords):
-        trapdoor = super().trapdoor(keywords)
-        super().server.search(super().pk, trapdoor)
+        super().upload([(self.consultant_pk, self.consultant_keyindex), (super().pk, super().keyindex)], document, keywords)
 
 
 class Server:
@@ -67,4 +61,4 @@ server = Server(mpeck)
 consultant = Consultant(mpeck, server)
 client = Client(mpeck, server, consultant)
 print(client.upload("text", ["a"]))
-print
+print(client.search(("a", 0)))
